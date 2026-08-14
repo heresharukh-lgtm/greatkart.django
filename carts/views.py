76b83ad59a55f177e1db729beb55c47eaba95066
shahrukh <heresharukh@gmail.com>
2026-08-14 21:17:06 +0500

    Fix cart view logic, duplicate functions, and cart template links

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from django.shortcuts import render, redirect
 from store.models import Product
 from .models import Cart,CartItem
-
+from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 
 # Create your views here.
@@ -36,6 +36,27 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total) / 100  # Example 2% tax
+        grand_total = total + tax            
+    except ObjectDoesNotExist:
+        pass #just ignore
+
+    
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/cart.html', context)
+
 
